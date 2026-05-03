6791ce4d377be767de8d38eef211dbe99cf05eb3
isra <cisra2002@gmail.com>
2026-05-03 00:20:59 -0500

    updated personal website

--- module_1/app/pages/routes.py
+++ module_1/app/pages/routes.py
@@ -1,3 +1,5 @@
+import json
+from pathlib import Path
 from flask import Blueprint, render_template
 
 # create a blueprint to organize page routes
@@ -10,10 +12,19 @@ def home():
 
 @pages_bp.get("/projects")
 def projects():
-    # projects page route
-    return render_template("projects.html", active_page="projects")
+    data_path = Path("data/projects.json")
+
+    with open(data_path, "r", encoding="utf-8") as f:
+        projects_data = json.load(f)
+
+    return render_template(
+        "projects.html",
+        projects=projects_data,
+        active_page="projects"
+    )
 
 @pages_bp.get("/contact")
 def contact():
     # contact page route
     return render_template("contact.html", active_page="contact")
+
